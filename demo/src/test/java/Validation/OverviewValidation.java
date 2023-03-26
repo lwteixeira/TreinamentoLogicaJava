4eef2e92135288ba8267cd70ff332881d65bb47a
Lucas Webber Teixeira <70859524+lwteixeira@users.noreply.github.com>
2023-03-26 18:52:14 -0300

    Update Validations

---
+++ demo/src/test/java/Validation/OverviewValidation.java
@@ -0,0 +1,30 @@
+package Validation;
+
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+
+import PageObjects.OverviewPage;
+
+public class OverviewValidation {
+
+    private WebDriver driver;
+    private OverviewPage overviewPage;
+    
+    public OverviewValidation(WebDriver driver) {
+        this.driver = driver;
+        overviewPage = new OverviewPage(this.driver);
+    }
+    
+    public void validationTotalPrince(String texto) {
+    	Assertions.assertEquals(texto, overviewPage.getTotalPrince().getText());
+    }
+    
+    public void validationFinishCompraImg() {
+        Assertions.assertTrue(overviewPage.getImgFinishOk().isDisplayed());
+    }
+    
+    public void validationTextFinish(String texto) {
+    	Assertions.assertEquals(texto, overviewPage.getTextFinishCompra().getText());
+    }
+    
+}
